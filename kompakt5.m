fac2bdf340a3ea26f178e25c7a0e8488912abcae
dtomasch29 <daniel.tomaschewski@tu-dortmund.de>
2016-04-13 16:06:17 +0200

    Added files via upload

---
+++ kompakt5.m
@@ -0,0 +1,16 @@
+function y=kompakt5(m)
+%Params
+%m=Dimension
+
+%Funktion, die die Matrix An erstellt bei Eingabe von m
+B=speye(m*m);
+%erstellt die Matrix B_m
+B=sparse(diag(4*ones(1,m))+sparse(diag(-1*ones(1,m-1),-1))+sparse(diag(-1*ones(1,m-1),1)));	
+%erstellt m-dimensionale Einheitsmatrix
+C=speye(m);
+%erstellt m-dimensionale Matrix mit 1 auf den beiden Hauptnebendiagonalen
+D=sparse(diag(ones(1,m-1),1)+diag(ones(1,m-1),-1));
+%erstellt die Matrix An durch Anwendung der Kronecker-Funktion		
+A=kron(C,B)+ kron(D,-speye(m));								
+%Rückgabe von A
+y=A;	
