afda06acca8acbe9a9e583ebe68e8dd126060b85
Christoph Höppke <christoph.hoeppke@math.tu-dortmund.de>
2016-05-20 14:43:36 +0200

    klaösdjf
    Signed-off-by: Christoph Höppke <christoph.hoeppke@math.tu-dortmund.de>

--- datacollector.sh
+++ datacollector.sh
@@ -1,29 +1,113 @@
 #!/bin/bash
-
 declare -a arrLevelH=("2" "3" "4" "5" "6" "7" "8");
 declare -a arrMaxIters=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10");
 declare -a arrTol=("5" "6" "7" "8");
 declare -a memtypeArray=("main" "cuda");
-declare -a memtypeArray2=("1" "2");
+declare -a compArray=("time" "h0err" "h1err");
 ## now loop through the above array
-for lvlH in "${arrLevelH[@]}"
-do
+
 echo "create file for level $lvlH ..."
-touch level_$lvlH.data
-    for lvlL in "${arrLevelL[@]}"
+lvlL=$((1));
+for miter in "${arrMaxIters[@]}"
+do
+    for tol in "${arrTol[@]}"
     do
-    for miter in "${arrMaxIters[@]}"
-    do
-        for tol in "${arrTol[@]}"
+        for mem in "${memtypeArray[@]}"
         do
-            for mem in "${memtypeArray2[@]}"
+            fileout=arryn-maxiters$miter-tol$tol-memtype$mem-mixprec.data;
+            touch $fileout;
+            echo "output file: $fileout";
+            echo "n time h0err h1err" > $fileout
+            for lvlH in "${arrLevelH[@]}"
             do
-                ./collector $lvlH $lvlL $miter $tol $mem
+                fldr=lvl$lvlH-1-miter$miter-tol$tol-mem$mem-mixprec;
+                time=$(cat $fldr/output.data | grep 'Total time:' | cut -d " " -f 3)
+                time=$(printf "%1.16f\n" $time)
+                h0error=$(cat $fldr/output.data | grep 'H0-Error:' | cut -d " " -f 2)
+                h0error=$(printf "%1.16f\n" $h0error)
+                h1error=$(cat $fldr/output.data | grep 'H1-Error:' | cut -d " " -f 2)
+                h1error=$(printf "%1.16f\n" $h1error)
+                n=$(bc <<< "2^$lvlH -1 " )
+                #a=grep 'Total time:' $fldr/output.data
+                #time=cut -d " " -f 2 <<< $a;
+                
+                #a=cat $fldr/output.data | grep ''
+                #h0error=cut -d " " -f 2 <<< $a; 
+                
+                #a=cat $fldr/output.data | grep 'H1-Error:'
+                #h1error=cut -d " " -f 2 <<< $a;
+                #n=bc <<< "2^$lvlH";
+                echo "$n $time $h0error $h1error" >> $fileout;
             done
         done
     done
-    done
-echo "done with collecting for level $lvlH..."
 done
-echo "ready! data can be found in level_X.data, choose wisely young padawan :"
-ls | grep "level_"
+
+for mem in "${memtypeArray[@]}"
+do
+    fileout=arryn-memtype$mem-singleprec.data;
+    rm $fileout;
+    touch $fileout;
+    echo "output file: $fileout";
+    echo " " > $fileout
+    echo "n time h0err h1err" > $fileout
+    for lvlH in "${arrLevelH[@]}"
+    do
+       fldr=lvl$lvlH-1-mem$mem-precsingle;
+       echo $fldr
+       time=$(cat $fldr/output.data | grep 'Total time:' | cut -d " " -f 3 | cut -d "s" -f 1)
+       time=$(printf "%1.16f\n" $time)
+       h0error=$(cat $fldr/output.data | grep 'H0-Error:' | cut -d " " -f 2)
+       h0error=$(printf "%1.16f\n" $h0error)
+       h1error=$(cat $fldr/output.data | grep 'H1-Error:' | cut -d " " -f 2)
+       h1error=$(printf "%1.16f\n" $h1error)
+       n=$(bc <<< "2^$lvlH -1 ")
+                
+       echo "$n $time $h0error $h1error" >> $fileout;
+    done
+done
+echo "done creating files";
+
+echo "creating LaTeX output";
+ltxfile=output2.tex
+fldr="."
+touch $fldr/$ltxfile;
+echo " " > $fldr/$ltxfile;
+cat LaTeX/com1.txt >> $fldr/$ltxfile
+
+#for tol in "${arrTol[@]}"
+#do
+#    for miter in "${arrMaxIters[@]}"
+#    do
+        tol=5
+        miter=3
+        #Arryn
+        cat LaTeX/com3.txt >> $fldr/$ltxfile;
+        for comp in "${compArray[@]}"
+        do
+            cat LaTeX/com2.txt >> $fldr/$ltxfile;
+            echo "\addplot[solid,mark=triangle,mark size=2pt,chart1] table[x=n, y=$comp] {arryn-maxiters$miter-tol$tol-memtypecuda-mixprec.data};" >> $fldr/$ltxfile
+            echo "\addplot[solid,mark=square,mark size=2pt,chart2] table[x=n, y=$comp] {arryn-maxiters$miter-tol$tol-memtypemain-mixprec.data};" >> $fldr/$ltxfile
+            echo "\addplot[solid,mark=diamond,mark size=2pt,chart3] table[x=n, y=$comp] {arryn-memtypecuda-singleprec.data};" >> $fldr/$ltxfile
+            echo "\addplot[solid,mark=otimes,mark size=2pt,chart4] table[x=n, y=$comp] {arryn-memtypemain-singleprec.data};" >> $fldr/$ltxfile
+            
+        done
+        #JETSON
+        for comp in "${compArray[@]}"
+        do
+            cat LaTeX/com2.txt >> $fldr/$ltxfile;
+            echo "\addplot[solid,mark=triangle,mark size=2pt,chart1] table[x=n, y=$comp] {arryn-maxiters$miter-tol$tol-memtypecuda-mixprec.data};" >> $fldr/$ltxfile
+            echo "\addplot[solid,mark=square,mark size=2pt,chart2] table[x=n, y=$comp] {arryn-maxiters$miter-tol$tol-memtypemain-mixprec.data};" >> $fldr/$ltxfile
+            echo "\addplot[solid,mark=diamond,mark size=2pt,chart3] table[x=n, y=$comp] {arryn-memtypecuda-singleprec.data};" >> $fldr/$ltxfile
+            echo "\addplot[solid,mark=otimes,mark size=2pt,chart4] table[x=n, y=$comp] {arryn-memtypemain-singleprec.data};" >> $fldr/$ltxfile
+            
+        done
+        echo "\end{groupplot}" >>  $fldr/$ltxfile
+#    done
+#done
+echo "\end{tikzpicture}" >>$fldr/$ltxfile
+echo "\end{document}" >> $fldr/$ltxfile
+
+
+
+
---
+++ multirun_poisson_dirichlet_2d_mixprec.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+declare -a arrLevelH=("2" "3" "4" "5" "6" "7" "8");
+declare -a arrMaxIters=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10");
+declare -a arrTol=("5" "6" "7" "8");
+declare -a memtypeArray=("main" "cuda");
+## now loop through the above array
+
+#echo "Starting mixprec tests"
+#for lvlH in "${arrLevelH[@]}"
+#do
+#  lvlL=$((1));
+#  while [ $lvlL -lt $lvlH ]
+#  do
+#   for miter in "${arrMaxIters[@]}"
+#    do
+#       for tol in "${arrTol[@]}"
+#       do
+#         for memtype in "${memtypeArray[@]}"
+#         do
+#             ./poisson_dirichlet_2d_mixprec --meshfile ../data/meshes/unit-square-quad.xml --mem $memtype --vtk --logto output.data --statistics --level $lvlH $lvlL --maxiter $miter --deftol 1E-$tol > output.log
+#             #foulderName = "$lvlH_$lvlL_miter$miter_tol$tol"
+#             fldr=lvl$lvlH-$lvlL-miter$miter-tol$tol-mem$memtype-mixprec
+#             echo "creating folder $fldr"
+#             mkdir -p $fldr
+#             mv *.vtu $fldr
+#             mv *.pvtu $fldr
+#             mv *.data $fldr
+#             mv *.log $fldr#
+#
+#             cat $fldr/output.log | grep 'H0-Error' >> $fldr/output.data
+#            cat $fldr/output.log | grep 'H1-Error' >> $fldr/output.data
+#             echo "memtype: $memtype" >> $fldr/output.data
+#            echo "prec: mixprec" >> $fldr/output.data
+#        done
+#       done
+#    done
+#  lvlL=$(($lvlL+1));
+#  done
+#done
+echo "Mixprec tests done"
+echo "Starting singleprec tests"
+for lvlH in "${arrLevelH[@]}"
+do
+
+  lvlL=$((1));
+  while [ $lvlL -lt $lvlH ]
+  do
+
+    for memtype in "${memtypeArray[@]}"
+    do
+
+         echo "Testlauf: LvlH: $lvlH LvlL: $lvlL Memtype: $memtype Prectype: singleprec";
+         ./poisson_dirichlet_2d --meshfile ../data/meshes/unit-square-quad.xml --mem $memtype --vtk --statistics --level $lvlH $lvlL > output.log
+         #foulderName = "$lvlH_$lvlL_miter$miter_tol$tol"
+         fldr=lvl$lvlH-$lvlL-mem$memtype-precsingle
+         echo "creating folder $fldr";
+         mkdir -p $fldr
+         mv *.vtu $fldr
+         mv *.pvtu $fldr
+         #mv *.data $fldr
+         mv *.log $fldr
+         touch $fldr/output.data
+         rm $fldr/output.data
+         touch $fldr/output.data
+         echo "Level-Grob:$lvlH" >> $fldr/output.data
+         echo "Level-Fine:$lvlL" >> $fldr/output.data
+         cat $fldr/output.log | grep 'Total time:' >>$fldr/output.data
+         cat $fldr/output.log | grep 'H0-Error' >> $fldr/output.data
+         cat $fldr/output.log | grep 'H1-Error' >> $fldr/output.data
+         echo "memtype: $memtype" >> $fldr/output.data
+         echo "prec: singleprec" >> $fldr/output.data
+    done
+    lvlL=$(($lvlL+1));
+  done
+done
+
+
+# You can access them using echo "${arr[0]}", "${arr[1]}" also
